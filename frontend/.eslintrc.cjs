c3db2459716596e55e21ac7471a328b0b01be7e7
Vojtěch Ferák <vferak@gmail.com>
2022-04-09 20:49:54 +0200

    Cypress init

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -12,4 +12,14 @@ module.exports = {
   env: {
     "vue/setup-compiler-macros": true,
   },
+  overrides: [
+    {
+      "files": [
+        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
+      ],
+      "extends": [
+        "plugin:cypress/recommended"
+      ]
+    }
+  ]
 };
